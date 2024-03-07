469604101256377e71202c395b859d9d2d1bc267
Colin Ian King <colin.i.king@intel.com>
2024-03-07 22:15:05 +0000

    BACKPORT: binder: remove redundant variable page_addr
    Variable page_addr is being assigned a value that is never read. The
    variable is redundant and can be removed.
    
    Cleans up clang scan build warning:
    warning: Value stored to 'page_addr' is never read [deadcode.DeadStores]
    
    Bug: 254441685
    Signed-off-by: Colin Ian King <colin.i.king@intel.com>
    Fixes: 162c79731448 ("binder: avoid user addresses in debug logs")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202312060851.cudv98wG-lkp@intel.com/
    Acked-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20240307221505.101431-1-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 367b3560e10bbae3660d8ba4d0a7cc92170d8398)
    [Lee: Surrounding code was different - no code changes]
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I99b2be6afa17036c200cee0d18aafdaaa88c6c0e

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -911,7 +911,6 @@ void binder_alloc_deferred_release(struct binder_alloc *alloc)
 		int i;
 
 		for (i = 0; i < alloc->buffer_size / PAGE_SIZE; i++) {
-			unsigned long page_addr;
 			bool on_lru;
 
 			if (!alloc->pages[i].page_ptr)
@@ -919,11 +918,6 @@ void binder_alloc_deferred_release(struct binder_alloc *alloc)
 
 			on_lru = list_lru_del(&binder_alloc_lru,
 					      &alloc->pages[i].lru);
-			page_addr = alloc->buffer + i * PAGE_SIZE;
-			binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
-				     "%s: %d: page %d at %lx %s\n",
-				     __func__, alloc->pid, i, page_addr,
-				     on_lru ? "on lru" : "active");
 			__free_page(alloc->pages[i].page_ptr);
 			page_count++;
 		}

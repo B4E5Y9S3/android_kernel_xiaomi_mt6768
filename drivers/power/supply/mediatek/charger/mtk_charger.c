ef380f138a2e879947e1864a2793c2e52e0d1082
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-01-19 07:54:34 +0000

    Silence "Vbat=3688,Ibat=-5906,I=0,VChr=28,T=31,Soc=34:38,CT:0:0 hv:1 pd:0:0"

--- drivers/power/supply/mediatek/charger/mtk_charger.c
+++ drivers/power/supply/mediatek/charger/mtk_charger.c
@@ -2087,6 +2087,7 @@ static int charger_routine_thread(void *arg)
 		bat_current = battery_get_bat_current();
 		chg_current = pmic_get_charging_current();
 		hvdcp_vbus_check(info);
+#ifdef CONFIG_MTK_ENG_BUILD
 		chr_err("Vbat=%d,Ibat=%d,I=%d,VChr=%d,T=%d,Soc=%d:%d,CT:%d:%d hv:%d pd:%d:%d\n",
 			battery_get_bat_voltage(), bat_current, chg_current,
 			battery_get_vbus(), battery_get_bat_temperature(),
@@ -2094,6 +2095,7 @@ static int charger_routine_thread(void *arg)
 			mt_get_charger_type(), info->chr_type,
 			info->enable_hv_charging, info->pd_type,
 			info->pd_reset);
+#endif
 
 		if (info->pd_reset == true) {
 			mtk_pe40_plugout_reset(info);
